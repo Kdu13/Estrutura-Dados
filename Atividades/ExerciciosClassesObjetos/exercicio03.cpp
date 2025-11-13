039e4d1a0b5de10d7cb23e6d660c74fbed308734
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-11-13 17:08:54 -0300

    Update exercicio03.cpp

--- Atividades/ExerciciosClassesObjetos/exercicio03.cpp
+++ Atividades/ExerciciosClassesObjetos/exercicio03.cpp
@@ -1,5 +1,3 @@
-// 3-) Crie uma classe Estacionamento que tenha como atributos da classe dia, horaEntrada, horaSaida, e dois métodos o primeiro Solicita os dados para o usuário e o outro método calcule o valor a ser pago sendo que cada hora equivale a 5 reais.
-
 #include <iostream>
 using namespace std;
 
